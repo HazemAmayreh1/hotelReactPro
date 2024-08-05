e2cafe60f2297b641f282a052b372af09e8aba83
HazemAmayreh1 <hazemamayreh44@gmail.com>
2024-08-05 18:38:08 +0300

    initPro

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Footer from './component/Footer/Footer'
+
+export default function App() {
+  return (
+    <Footer/>
+  )
+}
---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <>
+      <button className={style.error}>Error Button</button>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default Footer;
