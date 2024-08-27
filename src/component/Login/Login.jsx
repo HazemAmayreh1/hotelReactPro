0d12e8c591ff4cdaa88e8f55db0e00a74819732a
HazemAmayreh1 <hazemamayreh44@gmail.com>
2024-08-27 18:30:43 +0300

    handel submit login && reg

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,9 +1,61 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
-import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState(""); 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      console.log("Token found:", storedToken); 
+      navigate("/"); 
+    }
+  }, [navigate]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const encodedEmail = encodeURIComponent(email);
+    const encodedPassword = encodeURIComponent(password);
+
+    const apiUrl = `http://hotelmanagement.somee.com/api/GuestAPI/login/${encodedEmail}/${encodedPassword}`;
+
+    try {
+      const response = await axios.get(apiUrl);
+
+      if (response.status == 200) {
+        console.log("API call successful. Status 200 OK.");  
+        const responseData = response.data;
+
+        if (responseData.success || responseData.token) {
+          console.log("Login successful. Token:", responseData.token);  
+          setToken(responseData.token); 
+          localStorage.setItem("token", responseData.token);
+          toast.success(`Login successful! Token: ${responseData.token}`);
+          navigate("/");  
+        } else {
+          toast.error("Login failed: Invalid credentials.");
+        }
+      } else {
+        console.log(`Login failed with status: ${response.statusText}`); 
+        toast.error(`Login failed: ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error("Network Error:", error); 
+      toast.error(`Network Error: ${error.message}`);
+    }
+  };
+
   return (
     <section className="relative bg-gray-50 dark:bg-gray-900 overflow-hidden">
+      <ToastContainer />
       <div className="absolute inset-0" style={{ height: "100%" }}>
         <img
           src="https://wallpaperaccess.com/full/2690784.jpg"
@@ -16,8 +68,6 @@ function Login() {
         className="relative px-4 sm:px-6 py-4 mx-auto max-w-lg w-full"
         style={{ paddingTop: "2vh" }}
       >
-        {" "}
-        {/* Adjusted for responsiveness */}
         <div
           className="bg-white rounded-lg shadow dark:border xl:p-0 dark:bg-gray-800 dark:border-gray-700"
           style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
@@ -26,7 +76,7 @@ function Login() {
             <h1 className="text-lg sm:text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="email"
@@ -41,6 +91,8 @@ function Login() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                   placeholder="HotelLunar@gmail.com"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div>
@@ -57,14 +109,16 @@ function Login() {
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                   required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <button
                 type="submit"
-                className="w-full bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-bold rounded-lg  text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                className="w-full bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
               >
                 Sign in
-              </button>{" "}
+              </button>
               <p className="">
                 Don’t have an account yet?{" "}
                 <Link
--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,9 +1,52 @@
-import styles from "./Register.module.css";
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 
+
 function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const encodedEmail = encodeURIComponent(email);
+    const encodedPassword = encodeURIComponent(password);
+
+    
+    const apiUrl = `http://HotelManagement.somee.com/api/GuestAPI/register/${username}/${encodedEmail}/${encodedPassword}`;
+    try {
+      const response = await axios.get(apiUrl);
+
+      if (response.status === 200) {
+        toast.success("Registration successful!", {
+         
+        });
+      } else {
+        toast.error(`Registration failed: ${response.statusText}`, {
+          
+        });
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        toast.error("Unauthorized: Please check your credentials.", {
+          
+        });
+      } else {
+        toast.success("Registration successful!", {
+          
+        });
+      }
+      console.error("Error occurred:", error);
+    }
+  };
+
   return (
     <section className="relative overflow-hidden bg-gray-50 dark:bg-gray-900">
+      <ToastContainer />
       <div className="absolute inset-0" style={{ height: "100%" }}>
         <img
           src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?cs=srgb&dl=pexels-pixabay-258154.jpg&fm=jpg"
@@ -21,10 +64,10 @@ function Register() {
           style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}
         >
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-lg sm:text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+            <h1 className="text-lg sm:text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Create an account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="username"
@@ -39,12 +82,14 @@ function Register() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                   placeholder="YourName"
                   required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
               <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Your email
                 </label>
@@ -55,12 +100,14 @@ function Register() {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                   placeholder="HotelLunar@gmail.com"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Password
                 </label>
@@ -71,6 +118,8 @@ function Register() {
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                   required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <button
@@ -79,7 +128,7 @@ function Register() {
               >
                 Create an account
               </button>
-              <p className="">
+              <p>
                 Already have an account?{" "}
                 <Link
                   to="/login"
